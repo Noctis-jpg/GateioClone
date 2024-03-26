1fbd4652e5397bbf1f8d43c65d7a3fb25018cf61
Yunus <52599021+Noctis-jpg@users.noreply.github.com>
2024-03-26 22:33:03 +0300

    HeaderStart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,106 @@
-import React from 'react'
-
+import React from 'react';
+import { FontAwesomeIcon } from '../../node_modules/@fortawesome/react-fontawesome';
+import { faCompass } from '../../node_modules/@fortawesome/free-solid-svg-icons';
 const Header = () => {
   return (
-    <div>Header yunus</div>
-  )
-}
+    <header>
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-md-8 pad-0 d-flex ym_gap'>
+            <div className='Logo'>
+              <img width="120" height="auto" src="/logogate.png" alt="" />
+            </div>
+            <div className='Buttons'>
+              <a href='#' className='ActiveBtn'>Alım Satım</a>
+              <a href='#'>Web3</a>
+            </div>
+            <div className='SVGicons'>
+            <FontAwesomeIcon icon={faCompass} />
+            <div className="sub-menu large-sub-menu">
+                            <div className="container pad-r0">
+                                <div className="row">
+                                    <div className="col-md-4">
+                                      <span className='littleHead'>Alım-Satım</span>
+                                        <ul className="sub-menu-list">
+                                        <li><a href="#">Ortaklık Programı
+                                              <p>Gate.io'yu hedef kitlenizle paylaşarak komisyon kazanın</p>
+                                            </a></li>
+                                            <li><a href="#">Ortaklık Programı
+                                              <p>Gate.io'yu hedef kitlenizle paylaşarak komisyon kazanın</p>
+                                            </a></li>
+                                            <li><a href="#">Ortaklık Programı
+                                              <p>Gate.io'yu hedef kitlenizle paylaşarak komisyon kazanın</p>
+                                            </a></li>
+                                            <li><a href="#">Ortaklık Programı
+                                              <p>Gate.io'yu hedef kitlenizle paylaşarak komisyon kazanın</p>
+                                            </a></li>
+                                            <li><a href="#">Ortaklık Programı
+                                              <p>Gate.io'yu hedef kitlenizle paylaşarak komisyon kazanın</p>
+                                            </a></li>
+                                            <li><a href="#">Ortaklık Programı
+                                              <p>Gate.io'yu hedef kitlenizle paylaşarak komisyon kazanın</p>
+                                            </a></li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-md-4">
+                                    <span className='littleHead'>Promosyonlar</span>
+                                        <ul className="sub-menu-list">
+                                        <li><a href="#">Startup Listeleme
+                                              <p>Token lansman ve airdrop platformu</p>
+                                            </a></li>
+                                            <li><a href="#">Startup Listeleme
+                                              <p>Token lansman ve airdrop platformu</p>
+                                            </a></li>
+                                            <li><a href="#">Startup Listeleme
+                                              <p>Token lansman ve airdrop platformu</p>
+                                            </a></li>
+                                            <li><a href="#">Startup Listeleme
+                                              <p>Token lansman ve airdrop platformu</p>
+                                            </a></li>
+                                            <li><a href="#">Startup Listeleme
+                                              <p>Token lansman ve airdrop platformu</p>
+                                            </a></li>
+                                            <li><a href="#">Startup Listeleme
+                                              <p>Token lansman ve airdrop platformu</p>
+                                            </a></li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-md-4">
+                                    <span className='littleHead'>Öğren & Keşfet</span>
+                                        <ul className="sub-menu-list">
+                                            <li><a href="#">Alım-Satım
+                                              <p>1.700'den fazla kripto para birimi alıp satın</p>
+                                            </a></li>
+                                            <li><a href="#">Alım-Satım
+                                              <p>1.700'den fazla kripto para birimi alıp satın</p>
+                                            </a></li>
+                                            <li><a href="#">Alım-Satım
+                                              <p>1.700'den fazla kripto para birimi alıp satın</p>
+                                            </a></li>
+                                            <li><a href="#">Alım-Satım
+                                              <p>1.700'den fazla kripto para birimi alıp satın</p>
+                                            </a></li>
+                                            <li><a href="#">Alım-Satım
+                                              <p>1.700'den fazla kripto para birimi alıp satın</p>
+                                            </a></li>
+                                            <li><a href="#">Alım-Satım
+                                              <p>1.700'den fazla kripto para birimi alıp satın</p>
+                                            </a></li>
+                                        </ul>
+                                    </div>
+                  
+                                </div>
+                            </div>
+                         </div>
+            </div>
+          </div>
+          <div className='col-md-4'>
+ 
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,8 +3,8 @@ import Footer from './Footer';
 
 const RootLayout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen mx-auto max-w-2xl px-4 pt-8 pb-16">
-      <div className="flex-grow">
+    <div className="">
+      <div className="">
         <Header />
         <main className="my-0 py-16">{children}</main>
       </div>
